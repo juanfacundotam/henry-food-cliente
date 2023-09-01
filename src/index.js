a1e465feeda3550e3d3d6c35855ac90ffba0e6ed
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-09-01 11:49:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import axios from "axios";
 
 
 // axios.defaults.baseURL = "http://localhost:3001";
-axios.defaults.baseURL = "https://food-server-ubqd.onrender.com";
+axios.defaults.baseURL = "https://food-server-ubqd.onrender.com/";
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
